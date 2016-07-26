165e9386eae31f29bf76fd69d4259614e1c21e73
Dan Gorman <danielhgorman@gmail.com>
2016-07-26 18:23:03 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,15 +1,35 @@
 class CrewMember {
-  constructor(data) {
-    this.position = data.position;
-    this.id = data.id;
+  constructor(obj) {
+    this.position = obj.position;
+    this.id = obj.id;
     Store.data.crewMembers.push(this)
     this.currentShip = "Looking for a Rig"
+    this.spaceshipId = null
   }
-
   assignShip(ship) {
     this.currentShip = ship
+    this.spaceshipId = ship.id
+    ship.docked = false
   }
-
-
-
+  engageWarpDrive() {
+    if (this.currentShip === "Looking for a Rig" || this.position != "Pilot" ){
+      return "had no effect"
+    } else {
+      this.currentShip.warpDrive = 'engaged!'
+    }
+  }
+  setsInvisibility() {
+    if (this.currentShip === "Looking for a Rig" || this.position != "Defender"){
+      return "had no effect"
+    } else {
+      this.currentShip.cloaked = true
+    }
+  }
+  chargePhasers() {
+    if (this.currentShip === "Looking for a Rig" || this.position != "Gunner"){
+      return "had no effect"
+    } else {
+      this.currentShip.phasersCharge = 'charged!'
+    }
+  }
 }
--- spaceship.js
+++ spaceship.js
@@ -4,6 +4,13 @@ class Spaceship {
     this.name = obj.name;
     this.phasers = obj.phasers
     this.shields = obj.shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.phasers = 5
+    this.shields = 4
+    this.phasersCharge = 'uncharged'
+    this.docked = true
+
     Store.data.spaceships.push(this)
   }
 }
